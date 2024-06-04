efd921a0d736d1ab7b846631a836fcfb9a7da747
Shivansh Soni <147100730+Shivansh788@users.noreply.github.com>
2024-06-04 22:46:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+            int sh = 34;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("age :"+ sh);
+            // Console.ReadLine();
+        }
+    }
+}
